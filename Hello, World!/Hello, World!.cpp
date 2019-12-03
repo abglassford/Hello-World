7d417a248abb299d653053b87e72336154b18754
glassford <alex.glassford@bcsisims.com>
2019-12-03 14:44:53 -0700

    try this 3.3

--- Hello, World!/Hello, World!.cpp
+++ Hello, World!/Hello, World!.cpp
@@ -2,8 +2,16 @@
 
 int main()
 {
-    cout << "Hello, World!\n";
-    cout << "Here we go!\n";
+
+    cout << "Please enter your first name and age\n";
+    string first_name;
+    double age; 
+    cin >> first_name;
+    cin >> age;
+
+    double monthsAge = age * 12;
+
+    cout << "Hello, " << first_name << " (age " << monthsAge << ")\n";
 
     keep_window_open();
 
